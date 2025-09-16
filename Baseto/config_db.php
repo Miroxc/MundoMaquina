b2ff239782ff49219d59f0b72760487f38aa4001
Miroxc <91773538+Miroxc@users.noreply.github.com>
2025-09-15 22:13:57 -0300

    Update config_db.php

--- Baseto/config_db.php
+++ Baseto/config_db.php
@@ -6,10 +6,10 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Configuración de la base de datos
-$db_host = "sql209.infinityfree.com";
-$db_user = "if0_39482482";
-$db_pass = "j9fIXe3iyQEh0RX";
-$db_name = "if0_39482482_usuarios_web";
+$db_host = "";
+$db_user = "";
+$db_pass = "";
+$db_name = "";
 
 // Conexión
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
